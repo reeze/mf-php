e1ae70005f2efa5db1e8995f0a0ddeaa043ef34d
Reeze <reeze.xia@gmail.com>
2009-06-14 00:52:39 +0800

    Move the test application to sample folder

--- Mf.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require 'vendor/Mf/Mf.php';
-
-define('ROOT_DIR', dirname(__FILE__));
---
+++ sample/Mf.php
@@ -0,0 +1,7 @@
+<?php
+
+define('ROOT_DIR', dirname(__FILE__));
+
+
+// include the core mf framework
+require_once ROOT_DIR . DIRECTORY_SEPARATOR . '../lib/Mf.php';
